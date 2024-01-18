5d7c049c1a3648ad22336793501639842967a382
Eoin O'Neill <eoinoneill1991@gmail.com>
2024-01-18 14:16:28 -0800

    Expirmental: Adding optional ability to compile as godot module.
    Problems:
    - Header files are incorrect, should be adaptable.
    - register_types has to be made from scratch.

---
+++ module/register_types.h
@@ -0,0 +1,9 @@
+#ifndef HDR_REGISTER_TYPES_H
+#define HDR_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+void initialize_lua_module(ModuleInitializationLevel p_level);
+void uninitialize_lua_module(ModuleInitializationLevel p_level);
+
+#endif // HDR_REGISTER_TYPES_H
